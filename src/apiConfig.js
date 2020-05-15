5b906e4d98e3831a37d12013a68488f561643c55
Kane <Pperson599@gmail.com>
2020-05-15 09:12:56 -0400

    Config url fix

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://javasynth-api.herokuapp.com/',
+  production: 'https://javasynth-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
